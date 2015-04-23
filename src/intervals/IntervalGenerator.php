cd352583417682a8d021716a429d1f8e25f3917f
Saemie <saemie@chouchane.com>
2015-04-23 15:29:45 +0100

    Interval generator progress

--- src/intervals/IntervalGenerator.php
+++ src/intervals/IntervalGenerator.php
@@ -8,5 +8,24 @@
 namespace Hola\Intervals;
 
 class IntervalGenerator {
+  protected $planet_directory;
+  protected $planet;
 
+  function __construct() {
+    $this->planet_directory = __DIR__ . "/../assets/planets/";
+  }
+
+  /**
+   * Loads the configurations for a given planet.
+   */
+  public function LoadPlanetData($planet_id) {
+
+  }
+
+  /**
+   * Returns the data for loaded planet.
+   */
+  public function getPlanetData() {
+
+  }
 }
